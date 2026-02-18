2cd0f13d7ca33439f5dd992df6c08e335e151051
Melon <ribudidze@gmail.com>
2026-02-18 13:49:19 +0200

    solution_1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,66 @@
+from typing import Any, Optional
+
+
 class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot:
+    def __init__(self, name: str, weight: int, coords: Any=None):
+        if coords is None:
+            coords = [0, 0]
+        self.name = name
+        self.weight = weight
+        self.name = name
+        self.weight = weight
+        self.coords = coords
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+    def go_forward(self, step: int=1) -> None:
+        self.coords[1] += step
+
+    def go_back(self, step: int=1) -> None:
+        self.coords[1] -= step
+
+    def go_right(self, step: int=1) -> None:
+        self.coords[0] += step
+
+    def go_left(self, step: int=1) -> None:
+        self.coords[0] -= step
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name: str, weight: int, coords: Any=None):
+        super().__init__(name, weight, coords)
+        if coords is None:
+            coords = [0, 0, 0]
+        self.coords = coords
+
+    def go_up(self, point: int=1) -> None:
+        self.coords[2] += point
+
+    def go_down(self, point: int=1) -> None:
+        self.coords[2] -= point
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self,max_load_weight: Optional[int],
+                 current_load: Cargo | None,
+                 name: str,
+                 weight: int,
+                 coords: Any=None) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = None
+        if current_load is not None:
+            self.hook_load(current_load)
+
+    def hook_load(self, cargo: Cargo) -> None:
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+
+    def unhook_load(self) -> None:
+        self.current_load = None
